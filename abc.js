d23f9b033dc6286bdc2aaebd48d81f79d8d0129a
潘霄鹏 <2423200363@qqcom>
2020-01-30 09:28:46 +0800

    .

--- abc.js
+++ abc.js
@@ -2,7 +2,7 @@
 $(function () {
 	var start_time =
 //1
-"2020.01.28 24:00";
+"2020.01.29 24:00";
 //2
 	$("#start").text(start_time);
 	
@@ -23,18 +23,19 @@ $(function () {
             $("#info").text("请输入日期！");
 			$("#jgsj").text("全国确诊人数预测：- 人");
 		}else{
-			var i2 = new Date(i1);
-			var days = i2.getTime()-new Date('2020/01/13').getTime();
+			var i2 = new Date(i1+" 00:00");
+			var days = i2.getTime()-new Date('2020/01/13 00:00').getTime();
+			var days2 = new Date('2020/04/01 00:00').getTime()-i2.getTime();
 			var x = parseInt(days / (1000 * 60 * 60 * 24));
-			if(days>=0){
+			if(days>=0 && days2>=0){
 				var y=
 //3
-87.0076109391143-228.377330912115*x+134.300168194805*x*x-25.2639442922914*x*x*x+1.92968812018731*x*x*x*x-0.044334464555052*x*x*x*x*x;
+95.4468524252003-256.051143129338*x+146.763515122655*x*x-26.9954726323736*x*x*x+2.0024509803922*x*x*x*x-0.044298741763913*x*x*x*x*x;
 //4
 				$("#info").text("完成！");
 				$("#jgsj").text("全国确诊人数预测："+parseInt(y)+" 人");
 			}else{
-				$("#info").text("日期必须大于 2020-01-13");
+				$("#info").text("日期必须大于 2020-01-13 且小于 2020-04-01");
 				$("#jgsj").text("全国确诊人数预测：- 人");
 			}
 		}
